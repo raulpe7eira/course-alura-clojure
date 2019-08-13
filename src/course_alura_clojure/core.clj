8e1c212e20ccf806d66f6d7fe21a24c41ceb0664
Raul Pereira <raul.pereira@gmail.com>
2019-08-12 23:50:38 -0300

    Listas e suas funções

--- src/course_alura_clojure/core.clj
+++ src/course_alura_clojure/core.clj
@@ -3,14 +3,21 @@
 
 (def total-de-vidas 6)
 
-(defn perdeu [] (print "Você perdeu"))
+(defn perdeu [] (print "Você perdeu!"))
+(defn ganhou [] (print "Você ganhou!"))
 
-(defn jogo [vidas]
+(defn letras-faltantes [palavra acertos]
+  (remove (fn [letra] (contains? acertos (str letra))) palavra))
+
+(defn acertou-a-palavra-toda? [palavra acertos]
+  (empty? (letras-faltantes palavra acertos)))
+
+(defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
-    (do
-      (print vidas)
-      (jogo (- vidas 1)))))
+    (if (acertou-a-palavra-toda? palavra acertos)
+      (ganhou)
+      (print "Chuta, amigo!"))))
 
 (defn -main
   "I don't do a whole lot ... yet."
