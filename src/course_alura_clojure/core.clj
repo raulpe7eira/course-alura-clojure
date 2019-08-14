032f38ba860ac25037ee656ffe9dafa53acd74f0
Raul Pereira <raul.pereira@gmail.com>
2019-08-14 00:26:45 -0300

    Lendo do teclado e relacionamento entre funções

--- src/course_alura_clojure/core.clj
+++ src/course_alura_clojure/core.clj
@@ -12,12 +12,21 @@
 (defn acertou-a-palavra-toda? [palavra acertos]
   (empty? (letras-faltantes palavra acertos)))
 
+(defn le-letra! [] (read-line))
+
+(defn acertou? [chute palavra] (.contains palavra chute))
+
+(defn avalia-chute [chute vidas palavra acertos]
+  (if (acertou? chute palavra)
+    (jogo vidas palavra (conj acertos chute))
+    (jogo (dec vidas) palavra acertos)))
+
 (defn jogo [vidas palavra acertos]
   (if (= vidas 0)
     (perdeu)
     (if (acertou-a-palavra-toda? palavra acertos)
       (ganhou)
-      (print "Chuta, amigo!"))))
+      (avalia-chute (le-letra!) vidas palavra acertos))))
 
 (defn -main
   "I don't do a whole lot ... yet."
